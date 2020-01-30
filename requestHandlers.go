1409c56a2019c310750eabd227829d4fd15e17c5
sou-chon <sou.chon@outlook.com>
2020-01-30 17:04:58 +0000

    adding white list token

--- requestHandlers.go
+++ requestHandlers.go
@@ -9,7 +9,7 @@ import (
 	"fmt"
 )
 
-func HandleGetResourceRequestEnv(s3ClientPool *s3Client.S3ClientPool) ( func(*routing.Context) error ) {
+func HandleGetResourceRequestEnv(s3ClientPool *s3Client.S3ClientPool, whitelistTokens []string) ( func(*routing.Context) error ) {
 	/* This handler checks permission + formats. The s3client function checks the existence of objects */
 	return func(ctx *routing.Context) error {
 		store := ctx.Param("store")
@@ -22,11 +22,21 @@ func HandleGetResourceRequestEnv(s3ClientPool *s3Client.S3ClientPool) ( func(*ro
 		}
 
 		/* check credentials */
+		queryParams := ctx.QueryArgs()
+		token := string(queryParams.Peek("token"))
+		for k, v := range whitelistTokens {
+			if (v == token) {
+				break
+			}
+			if (k == len(whitelistTokens) - 1) {
+				return fmt.Errorf("%v: Cannot get resource \"%v\". Invalid token. Please provide token in url as ?token={{token}}.", definitions.ACCESS_DENIED_ERROR, resource)
+			}
+		}
 
 
 		/* getting the file */
 		object, err, statusCode := s3ClientPool.DownloadFile(store, project, resource)
-		if (err != nil) {
+		if err != nil {
 			ctx.SetStatusCode(statusCode)
 			return fmt.Errorf("%v", err)
 		}
